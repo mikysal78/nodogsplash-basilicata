ea286849f1e2809406c60de62cb85c0eed8319ae
MikyTux <mikysal78@gmail.com>
2016-08-22 19:33:52 +0200

    Scripts per limitare l'accesso ad alcuni siti elencati nel file bannned.txt

---
+++ scripts/ban-domains.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for domain in `cat /shared/banned.txt`; do
+        dig @8.8.8.8 $domain | egrep [0-9] | grep IN| awk {'print $5'} >> /tmp/ips.txt
+done
+
+for ip in `cat /tmp/ips.txt`; do
+        iptables -t nat -I ndsOUT -p tcp -s 192.168.20.0/24 -d $ip --dport 80 -j DNAT --to 192.168.20.1:2050
+done
+
